a22c2e531c543092d84e227808f605d164e8ac3c
bangdaidai <thxambw@163.com>
2026-08-27 01:16:51 +0800

    Update AuthorDetailScreen.kt

--- app/src/main/java/io/legado/app/ui/main/my/authorManage/AuthorDetailScreen.kt
+++ app/src/main/java/io/legado/app/ui/main/my/authorManage/AuthorDetailScreen.kt
@@ -26,6 +26,7 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.input.nestedscroll.nestedScroll
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
